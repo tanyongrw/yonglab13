fe9f42d94894e714aead1805d0cea0d4373586c6
tanyongrw <rudjaya22934@gmail.com>
2023-01-10 15:34:59 +0700

    finish

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+int fibonacci (int);
 
 int main(){
     int x;
@@ -7,3 +8,14 @@ int main(){
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci(int x){
+    if(x > 1){
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
+    else if( x > 1 || x == 1){
+        return x;
+    }
+    return 0;
+}
+
